1f505ca626465c0202db725da42a8d9ee61c275e
XX <zhangyangliu@foxmail.com>
2020-09-03 10:53:48 +0800

    初始化项目

---
+++ DouYuCopySwift/DouYuCopySwiftTests/DouYuCopySwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  DouYuCopySwiftTests.swift
+//  DouYuCopySwiftTests
+//
+//  Created by yckj on 2020/9/3.
+//  Copyright © 2020 zxx. All rights reserved.
+//
+
+import XCTest
+@testable import DouYuCopySwift
+
+class DouYuCopySwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
